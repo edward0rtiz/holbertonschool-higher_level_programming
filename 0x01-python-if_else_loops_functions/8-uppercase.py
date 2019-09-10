5ea5103f1aedcee01cf6302253a3d4086d2630ad
edward0rtiz <931@holbertonschool.com>
2019-09-10 18:34:21 +0000

    [feature] add 8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")

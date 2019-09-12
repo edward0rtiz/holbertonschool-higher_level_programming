0d2b6e0cece281c0bb51dba6db99c36b731ee872
edward0rtiz <931@holbertonschool.com>
2019-09-12 02:49:59 +0000

    [fix] lenght of char too long before

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    built_op = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(built_op.keys()):
+    blt_op = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(blt_op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    print("{} {} {} = {}".format(a, sys.argv[2], b, built_op[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, blt_op[sys.argv[2]](a, b)))

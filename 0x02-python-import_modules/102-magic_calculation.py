7cb42bf0b1f7d807661755b12d1c4cff723848b1
edward0rtiz <931@holbertonschool.com>
2019-09-12 03:21:04 +0000

    [fix] remove dis module

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import dis
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
@@ -12,5 +10,3 @@ def magic_calculation(a, b):
 
     else:
         return(sub(a, b))
-
-dis.dis(magic_calculation)

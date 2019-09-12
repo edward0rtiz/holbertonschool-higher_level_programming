cbde21cd21ae9eeedf80971ad0f25a65e1a32dd1
edward0rtiz <931@holbertonschool.com>
2019-09-12 03:20:15 +0000

    [fix] fix return c indentation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import dis
+
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
@@ -6,7 +8,9 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return(c)
+        return(c)
 
     else:
         return(sub(a, b))
+
+dis.dis(magic_calculation)

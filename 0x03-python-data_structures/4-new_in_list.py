dff18d9a6d21865155f4a7d4d3589935c54e0392
edward0rtiz <931@holbertonschool.com>
2019-09-17 19:25:22 +0000

    [feature]add new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+        my_list_2 = [i for i in my_list]
+        my_list_2[idx] = element
+    return (my_list_2)

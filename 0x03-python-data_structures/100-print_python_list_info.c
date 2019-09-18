ab0044f0957640465f5e6d62cd1224db851b6920
edward0rtiz <931@holbertonschool.com>
2019-09-18 02:07:52 +0000

    [update] description

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <Python.h>
 
+/**
+ * print_python_list_info - Function that prints basic info about python objects
+ * @p: PyObject
+ */
+
 void print_python_list_info(PyObject *p)
 {
 	int size;

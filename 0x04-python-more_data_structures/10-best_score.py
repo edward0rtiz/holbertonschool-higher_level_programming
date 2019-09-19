1a087c16c9dfc2171f6a7a8d0e0f084e54f2c821
edward0rtiz <931@holbertonschool.com>
2019-09-19 19:00:11 +0000

    [fix]add shebang

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
+        return (None)
 
     rvalue = list(a_dictionary.keys())[0]
     b_score = list(a_dictionary.values())[0]

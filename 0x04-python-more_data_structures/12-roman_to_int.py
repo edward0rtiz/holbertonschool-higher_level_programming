d5e5a5376fe5c59bc304737c186892014086a550
edward0rtiz <931@holbertonschool.com>
2019-09-19 19:32:20 +0000

    [fix] code performace

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,15 +3,7 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or roman_string is None):
         return (0)
 
-    roman_d = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-    }
+    roman_d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = 0
 
     for i in range(len(roman_string)):

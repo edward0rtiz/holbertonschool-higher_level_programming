f5e09177b6af66585f969cb49b27b2caf06304eb
edward0rtiz <931@holbertonschool.com>
2019-09-19 18:52:04 +0000

    [fix]renmae var acording pep8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
+        return (None)
 
     rvalue = list(a_dictionary.keys())[0]
     b_score = a_dictionary[rvalue]
-    for i, j in a_dictionary.items():
-        if j > b_score:
-            b_score = j
-            r_value = i
+    for k, v in a_dictionary.items():
+        if v > b_score:
+            b_score = v
+            r_value = k
     return (r_value)

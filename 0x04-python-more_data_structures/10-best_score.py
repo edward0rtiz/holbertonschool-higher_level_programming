ef51b431766ec0e6bfa1411a494082da159b6395
edward0rtiz <931@holbertonschool.com>
2019-09-19 19:15:28 +0000

    [fix]ln 7 r_value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    rvalue = list(a_dictionary.keys())[0]
+    r_value = list(a_dictionary.keys())[0]
     b_score = list(a_dictionary.values())[0]
     for k, v in a_dictionary.items():
         if v > b_score:

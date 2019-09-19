e8e9103e791e86d6fb922dab342526f8c02d64e1
edward0rtiz <931@holbertonschool.com>
2019-09-19 18:53:30 +0000

    [fix] <= 0 stmt

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) <= 0:
         return (None)
 
     rvalue = list(a_dictionary.keys())[0]

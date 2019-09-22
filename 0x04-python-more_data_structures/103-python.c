397c1cd2111169a30de9337641085d81992230ff
edward0rtiz <931@holbertonschool.com>
2019-09-22 16:01:40 +0000

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -7,11 +7,15 @@
  * @p: PyObject
  */
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+
 void print_python_list(PyObject *p)
 {
 	int size;
 	int alloc;
-	int counter;
+	int b;
 	PyVarObject *var = (PyVarObject *)p;
 	PyListObject *list = (PyListObject *)p;
 	const char *type;
@@ -23,12 +27,46 @@ void print_python_list(PyObject *p)
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", alloc);
 
-	for (counter = 0; counter < size; counter++)
+	for (b = 0; b < size; b++)
 	{
-		type = list->ob_item[counter]->ob_type->tp_name;
-		printf("Element %d: %s\n", counter, type);
+		type = list->ob_item[b]->ob_type->tp_name;
+		printf("Element %d: %s\n", b, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[b]);
+	}
+}
 
-		/*object = PyList_GetItem(p, counter);
-		  printf("%s\n", Py_TYPE(object)->tp_name); */
+/**
+ * print_python_bytes - Fucntion that prints basic info of python bytes
+ * @p: Pyobject of byte CPython structure
+ */
+
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bt = (PyBytesObject *)p;
+	unsigned char size;
+	unsigned char c;
+
+	printf("[.] bytes object info/n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf(" [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	printf(" size: %ld\n", ((PyVarObject *)p)->ob_size);
+	printf(" trying string %s\n", bt->ob_sval);
+
+	if (((PyVarObject *)p)->ob_size > 10)
+		size = 10;
+	else
+		size = ((PyVarObject *)p)->ob_size + 1;
+	printf(" first %d bytes: ", size);
+	for (c = 0; c < size; c++)
+	{
+		printf("%02hhx", bt->ob_sval[c]);
+		if (c == (size - 1))
+			printf("\n");
+		else
+			printf(" ");
 	}
 }

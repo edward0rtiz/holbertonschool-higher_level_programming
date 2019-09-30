fabc7927fe113e3a648a92e6d9df6a267ca7ed16
edward0rtiz <931@holbertonschool.com>
2019-09-30 19:29:28 +0000

    [fix] remove else use continue instead, fix ndentation

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     return_element = 0
     for i in range(0, x):
-        try: 
-            print("{}".format(my_list[i]), end="")
+        try:
+            print("{:d}".format(my_list[i]), end="")
             return_element = return_element + 1
         except (ValueError, TypeError):
-        else
-        print("")
-        return (return_element)
+            continue
+    print("")
+    return (return_element)

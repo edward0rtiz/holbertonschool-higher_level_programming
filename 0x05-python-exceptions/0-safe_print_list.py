59d817d6b936fcf3cf1b307de566a10a86674fcc
edward0rtiz <931@holbertonschool.com>
2019-09-30 19:19:10 +0000

    [fix] line 7 remove else for break

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             return_element = return_element + 1
         except IndexError:
-            else
-            print("")
+            break
+    print("")
     return (return_element)

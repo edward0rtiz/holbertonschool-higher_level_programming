ba8c09afaae208a35db5a4fb9ec08ecc3c78559c
edward0rtiz <931@holbertonschool.com>
2019-09-30 19:09:57 +0000

    [feature] add 3-safe-print-division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        division = a / b
+    except (ZeroDivisionError, TypeError):
+        division = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (division)

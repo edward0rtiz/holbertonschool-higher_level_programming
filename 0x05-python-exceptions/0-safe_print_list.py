805768a1f55595c954953084d60984292a0082db
edward0rtiz <931@holbertonschool.com>
2019-09-30 19:08:26 +0000

    [feature] add 0-safeprintlist

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    return_element = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            return_element = return_element + 1
+        except IndexError:
+        else
+        print("")
+        return (return_element)

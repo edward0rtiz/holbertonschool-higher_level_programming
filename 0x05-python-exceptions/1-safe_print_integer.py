efc5b9db5dca56be30e452fd226d95713a1f48f4
edward0rtiz <931@holbertonschool.com>
2019-09-30 19:25:36 +0000

    [fix] using {:d} format

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{}".format(value))
+        print("{:d}".format(value))
         return (True)
-    except (TypeError, ValueError):
+    except (ValueError, TypeError):
         return (False)

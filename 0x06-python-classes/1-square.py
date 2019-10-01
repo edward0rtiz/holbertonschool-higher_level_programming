83a60fa652e9c919dfb7b83894311511cecd6ca9
edward0rtiz <931@holbertonschool.com>
2019-10-01 16:18:47 -0500

    [fix] fix ln 5 indentation code

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,9 @@
-
 #!/usr/bin/python3
 class Square:
     """type class square"""
     def __init__(self, size):
-    """Init the square classs
-    Args:
-     param1: size is the type int attribute to make it private
-    """
-    self.__size = size
+        """Init the square class
+        Args:
+        param1: size is the type int attribute to make it private
+        """
+        self.__size = size

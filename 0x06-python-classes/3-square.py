58fa2bddb7f8c4f362045fb24903e2717446e3bb
edward0rtiz <931@holbertonschool.com>
2019-10-01 16:22:19 -0500

    [fix] ln 11 statemnet elif

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,10 +8,10 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size mut be an integer")
-        else size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
         """Return the area of the square"""
-    return (self.__size * self.__size)
+        return (self.__size * self.__size)

d5477114808baa913117754973de343516d3ea6d
edward0rtiz <931@holbertonschool.com>
2019-10-01 16:39:10 -0500

    [fix] fix else

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
     @property
     def size(self):
@@ -23,7 +24,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
         """Return the area of the square"""

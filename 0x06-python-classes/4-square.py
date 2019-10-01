98ca959be4d14761fe01246d82406580145a8b06
edward0rtiz <931@holbertonschool.com>
2019-10-01 16:56:01 -0500

    [fix] add size=0

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+    """Type class square"""
+    def __init__(self, size=0):
+        """Init the square classs
+        Args:
+        param1: size is the type int attribute to make it private
+        """
+        self.size = size
 
     @property
     def size(self):
+        """get the size of the square"""
         return (self.__size)
 
     @size.setter

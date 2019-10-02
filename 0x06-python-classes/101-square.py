f933d6399f16dbebbb8f43b66074cf54a443d247
edward0rtiz <931@holbertonschool.com>
2019-10-01 21:32:05 -0500

    [fix] pep8 spaces

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 class Square:
-
-
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -24,7 +22,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or len (value) != 2 or
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
                 not all(isinstance(number, int) for number in value) or
                 not all(number >= 0 for number in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -38,8 +37,8 @@ class Square:
             print("")
             return
 
-        [print("") for i in  range(0, self.__position[1])]
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for  j in range(0, self.__position[0])]
+            [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")

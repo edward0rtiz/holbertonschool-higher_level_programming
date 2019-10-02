febecc41f9ebdebdfb4068954fbac01e56d29b22
edward0rtiz <931@holbertonschool.com>
2019-10-01 21:58:07 -0500

    [fix] ln 50 correct bloack and indentation

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -42,3 +42,13 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
+    def __str__(self):
+        if self.__size != 0:
+            [print("")for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")

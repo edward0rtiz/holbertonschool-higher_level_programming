563260e15a565b74a8716ac3b968696fed65ff95
edward0rtiz <931@holbertonschool.com>
2019-10-01 21:13:20 -0500

    [feature] test 101-square cheks #

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+class Square:
+
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len (value) != 2 or
+                not all(isinstance(number, int) for number in value) or
+                not all(number >= 0 for number in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in  range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for  j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")

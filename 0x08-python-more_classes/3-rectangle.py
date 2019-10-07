6f4df39f4213e98f91d2427bc6d8c8c2f274ce87
edward0rtiz <931@holbertonschool.com>
2019-10-07 11:23:04 -0500

    [feature]add 3-reclange using magic method str

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Class rectangle
+"""
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+
+        self.width =width
+        self.height = height
+
+    @property
+    def width(self):
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    def area(self):
+
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rectangle = []
+        for i in range(self.__height):
+            [rectangle.append("#") for j in range(self.__width)]
+            if i != self.__height -1:
+                rectangle.append("\n")
+        return ("".join(rectangle))

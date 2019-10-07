be6ed3a1c72d5a1ceae5b8933ea46fa9f5415431
edward0rtiz <931@holbertonschool.com>
2019-10-07 16:35:15 -0500

    [fix] pep8 ln 10 whitespace

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,11 +2,13 @@
 """
 Class rectangle
 """
+
+
 class Rectangle:
 
     def __init__(self, width=0, height=0):
 
-        self.width =width
+        self.width = width
         self.height = height
 
     @property

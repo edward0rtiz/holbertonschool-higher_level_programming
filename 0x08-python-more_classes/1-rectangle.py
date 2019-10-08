c89b5e62d51118008950ffdd51332812ef81b9fd
edward0rtiz <931@holbertonschool.com>
2019-10-07 19:05:49 -0500

    [fix] position of width and height print

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,6 +12,19 @@ class Rectangle:
         self.height = height
 
     @property
+    def height(self):
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    @property
     def width(self):
 
         return self.__width
@@ -25,15 +38,3 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    @property
-    def height(self):
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__height = value

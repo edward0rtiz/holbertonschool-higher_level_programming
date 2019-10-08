6ad3b14c30592b4b77c8ff7d8199162fabe21454
edward0rtiz <931@holbertonschool.com>
2019-10-08 18:40:07 -0500

    [fix] 101-nqueens fix indentation, arraganment declaration and iteration

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,32 +5,49 @@ import sys
 def solve(row, column):
     solver = [[]]
     for q in range(row):
-        solver = place_queen(q, column, row)
+        solver = place_queen(q, column, solver)
     return solver
 
-def place_queen(q, column, array):
+def place_queen(q, column, prev_solver):
     solver_queen = []
-    for array in solver_queen:
-        for x in range(rows):
+    for array in prev_solver:
+        for x in range(column):
             if is_safe(q, x, array):
-                solver_queen.append(array + x)
+                solver_queen.append(array + [x])
     return solver_queen
 
 def is_safe(q, x, array):
     if x in array:
         return (False)
     else:
-        return all(abs(array[column] - x) != q - columns
+        return all(abs(array[column] - x) != q - column
                    for column in range(q))
+
+
+def init():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        the_queen = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if the_queen < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return(the_queen)
 
 def n_queens():
 
+    the_queen = init()
     solver = solve(the_queen, the_queen)
     for array in solver:
         clean = []
         for q, x in enumerate(array):
             clean.append([q, x])
-            print(clean)
+        print(clean)
+
 
 if __name__ == '__main__':
     n_queens()

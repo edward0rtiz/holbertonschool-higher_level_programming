b114dda38b30caac27415f48717cfa827ff93a81
edward0rtiz <931@holbertonschool.com>
2019-10-07 19:32:30 -0500

    [fix] height position

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -31,6 +31,19 @@ class Rectangle:
         return (rect_2)
 
     @property
+    def height(self):
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
     def width(self):
 
         return self.__width
@@ -43,19 +56,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self):
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__height = value
 
     def area(self):
 

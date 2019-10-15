84143e2a0203c51067d425c9876b6c73c53543d8
edward0rtiz <931@holbertonschool.com>
2019-10-15 16:00:57 -0500

    [feature] add full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+
+        string = "[Rectangle] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string

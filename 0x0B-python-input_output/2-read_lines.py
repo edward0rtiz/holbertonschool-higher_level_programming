91eca712d26f0fc45dfdef3075881d65742fcd5d
edward0rtiz <931@holbertonschool.com>
2019-10-16 15:03:10 -0500

    [feature] add read line

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def read_lines(filename="", nb_lines=0):
+    with open(filename, encoding="UTF-8") as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
+            return
+
+        ln = 0
+        for line in f:
+            ln += 1
+        f.seek(0)
+        if nb_lines >= ln:
+            print(f.read(), end="")
+            return
+        else:
+            i = 0
+            while i < nb_lines:
+                print(f.readline(), end="")
+                i += 1

a2e10e9f232717dabe6e3862f0cb50532ccd6147
edward0rtiz <931@holbertonschool.com>
2019-10-15 20:33:04 -0500

    [fix] pep8

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
+
+    """function to check is obj is inherited in a class
+    Arguments:
+        param1: obj
+        param2: a_class that matches the obj
+    Return:
+    True for issubclass of obj or False if not
+    """
+
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:

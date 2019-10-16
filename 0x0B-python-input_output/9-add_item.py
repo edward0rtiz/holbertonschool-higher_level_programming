4eb4f6ae5360ba706c294bbe71763c8ac71a112f
edward0rtiz <931@holbertonschool.com>
2019-10-16 18:17:59 -0500

    [feature] add load, add, save handling exceptions

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+    try:
+        item = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        item = []
+    item.extend(sys.argv[1:])
+    save_to_json_file(item, "add_item.json")

199bbca0a3da0167998f78c04e5d87276f5a5ace
edward0rtiz <931@holbertonschool.com>
2019-10-15 20:31:43 -0500

    [fix} pep8

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class MyList(list):
+    """Type class MyList  with print_sorted function"""
+
     def print_sorted(self):
         print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
+    """function to check is obj is the same class
+    Arguments:
+        param1: obj
+        param2: a_class that matches the obj
+    Return:
+    True for isinstance of obj or False if not
+    """
+
     if type(obj) == a_class:
         return True
     else:

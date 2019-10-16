bccf1c3c1de3a47e8c400ced943f131f4550c837
edward0rtiz <931@holbertonschool.com>
2019-10-15 20:31:57 -0500

    [fix] pep8

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
+    """function to check is obj is the same kind of class
+    Arguments:
+        param1: obj
+        param2: a_class that matches the obj
+    Return:
+    True for isinstance of obj or False if not
+    """
+
     if isinstance(obj, a_class):
         return True
     else:

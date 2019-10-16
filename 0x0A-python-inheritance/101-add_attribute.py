ae180d26a775b334ca7dd525539311710cf2835c
edward0rtiz <931@holbertonschool.com>
2019-10-15 20:35:44 -0500

    [feature] add attribute fucntion using hasattr and getattr

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def add_attribute(obj, attribute, value):
+
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, value)

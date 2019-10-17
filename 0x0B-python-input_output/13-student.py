d610cc1b0ab4fcbbba5b7bf4a64b77773e3cbe4d
edward0rtiz <931@holbertonschool.com>
2019-10-16 23:08:42 -0500

    [fix]ln 14 long line

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -12,7 +12,8 @@ class Student:
 
     def to_json(self, attrs=None):
 
-        if (type(attrs) == list and all(type(element) == str for element in attrs)):
+        if (type(attrs) == list and all(type(element) == str
+                                        for element in attrs)):
             return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
         return self.__dict__
 

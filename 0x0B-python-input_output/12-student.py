caf0fd5a2c852a811a6fd6445d74dd20c33b96d3
edward0rtiz <931@holbertonschool.com>
2019-10-16 20:46:07 -0500

    [feature]add 12-student using getattr and hasattr dinamic programming introspection functions

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if(type(attrs) == list and all (type(element) == str for element in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__

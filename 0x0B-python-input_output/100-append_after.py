64612d67bca5e4639f319d7560bc1bcaebc5feb6
edward0rtiz <931@holbertonschool.com>
2019-10-16 21:35:05 -0500

    [feature]add append using write w

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def append_after(filename="", search_string="", new_string=""):
+
+    txt = ""
+    with open(filename) as r:
+        for line in r:
+            txt += line
+            if search_string in line:
+                txt += new_string
+    with open(filename, "w") as w:
+        w.write(txt)

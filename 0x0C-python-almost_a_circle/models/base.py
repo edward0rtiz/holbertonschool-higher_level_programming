c90f94b29682346f45f13cbc78a72a78dd8574e3
edward0rtiz <931@holbertonschool.com>
2019-10-20 17:31:45 -0500

    [fix] pep8 ln6, ln16, ln20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 import json
 import csv
 
+
 class Base:
     """Type class of a base model"""
 
@@ -13,9 +14,8 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects +=1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
 
     @staticmethod
     def to_json_string(list_dictionaries):

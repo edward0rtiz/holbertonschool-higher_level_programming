9393d4b1f2650a333b1cf41897b118d39025ea50
edward0rtiz <931@holbertonschool.com>
2019-10-19 19:04:28 -0500

    [update] add public method update *args and **kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,36 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for j, k in kwargs.items():
+                if j == "id":
+                    if k is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = k
+                elif j == "size":
+                    self.size = k
+                elif j == "x":
+                    self.x = k
+                elif j == "y":
+                    self.y = k
+                    
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)

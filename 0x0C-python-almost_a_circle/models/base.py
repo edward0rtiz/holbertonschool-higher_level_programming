52fb298e3f073ed851912e275c9d8249393f8ecf
edward0rtiz <931@holbertonschool.com>
2019-10-20 14:04:28 -0500

    [update] Json static method that returns JSON str of list dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import json
+
 class Base:
     """Type class of a base model"""
 
@@ -12,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects +=1
             self.id = Base.__nb_objects
+
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)

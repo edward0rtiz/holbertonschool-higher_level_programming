ac4502144067cddd86a27112d5bbfa62ab54f6b5
edward0rtiz <931@holbertonschool.com>
2019-10-22 16:34:44 -0500

    [fix] ln 24 and ln 34 empty add empty lists

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,8 +21,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
 
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return '[]'
+        if list_dictionaries is None or len(list_dictionaries) == []:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
@@ -31,10 +31,10 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, "w") as f:
             if list_objs is None:
-                f.write("")
+                f.write("[]")
             else:
-                list_dicts = [o.to_dictionary() for o in list_objs]
-                f.write(Base.to_json_string(list_dicts))
+                list_dictionaries = [o.to_dictionary() for o in list_objs]
+                f.write(Base.to_json_string(list_dictionaries))
 
     @staticmethod
     def from_json_string(json_string):

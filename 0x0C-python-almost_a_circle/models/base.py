49eaeba3722fba2cf497150e03f55d10718b6bc8
edward0rtiz <931@holbertonschool.com>
2019-10-22 16:23:39 -0500

    [fix] remove comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,7 +11,6 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-    """__init function"""
 
         if id is not None:
             self.id = id
@@ -21,7 +20,6 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """to_json_string function"""
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
@@ -29,7 +27,6 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """save_to_file function"""
 
         filename = cls.__name__ + '.json'
         with open(filename, 'w') as f:
@@ -41,7 +38,6 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """from_json_string function"""
 
         if json_string is None or json_string == '[]':
             return []
@@ -50,7 +46,6 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        """create function"""
 
         new_instance = cls(5, 5)
         new_instance.update(**dictionary)
@@ -58,7 +53,6 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """load_from_file function"""
 
         filename = str(cls.__name__) + '.json'
         try:
@@ -70,7 +64,6 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """save_to_file_csv function"""
 
         filename = cls.__name__ + '.csv'
         with open(filename, 'w', newline='') as csvfile:
@@ -87,7 +80,6 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
-        """load_from_file_csv"""
 
         filename = cls.__name__ + '.csv'
         try:

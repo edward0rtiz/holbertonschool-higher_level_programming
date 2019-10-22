ba69d9cc781aaf459b372ccfe42aac0823633dac
edward0rtiz <931@holbertonschool.com>
2019-10-22 16:25:41 -0500

    [fix] double qute in save_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,10 +28,10 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
 
-        filename = cls.__name__ + '.json'
-        with open(filename, 'w') as f:
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
             if list_objs is None:
-                f.write('')
+                f.write("")
             else:
                 list_dictionaries = [o.to_dictionary() for o in list_objs]
                 f.write(Base.to_json_string(list_dictionaries))

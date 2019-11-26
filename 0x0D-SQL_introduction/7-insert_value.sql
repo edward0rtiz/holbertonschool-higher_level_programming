ea33a3f7237c137558e75d8d127261941ed3ad67
edward0rtiz <931@holbertonschool.com>
2019-11-25 19:03:53 -0500

    [fix] add space ln 1

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---insert values at sql table
-INSERT INTO `first_table` (`id`,`name`) VALUES (89, "Holberton School");
+-- insert values at sql table
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");

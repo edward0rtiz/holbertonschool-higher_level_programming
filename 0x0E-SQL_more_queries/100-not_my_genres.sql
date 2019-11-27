3ba72c0ced5b3fb0695e467bbfff8826811ccd7a
edward0rtiz <931@holbertonschool.com>
2019-11-26 23:19:49 -0500

    [feature] Query to list genres not linked to Dexter show

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,16 @@
+--Query to list genres not linked to Dexter show
+SELECT DISTINCT name
+  FROM tv_genres AS genres
+	 INNER JOIN tv_show_genres AS show_genres
+	     ON genres.id = show_genres.genre_id
+	 INNER JOIN tv_shows AS shows
+	     ON show_genres.show_id = shows.id
+ WHERE genres.name NOT IN
+       (SELECT name
+	  FROM tv_genres AS genres
+		 INNER JOIN tv_show_genres AS show_genres
+		     ON genres.id = show_genres.genre_id
+		 INNER JOIN tv_shows AS shows
+		     ON show_genres.show_id = shows.id
+	 WHERE shows.title = 'Dexter')
+ORDER BY genres.name;

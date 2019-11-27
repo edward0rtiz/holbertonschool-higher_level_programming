6284a4f94ca533cfa2820410c31e8c8dd6dad79f
edward0rtiz <931@holbertonschool.com>
2019-11-26 19:52:04 -0500

    [fix] CREATE typo error

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- create a table id_unique with a unique default value
-CREEATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));

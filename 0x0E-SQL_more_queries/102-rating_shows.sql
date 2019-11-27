fef06b7686c454a14220b9b4f4e702204f36fe5e
edward0rtiz <931@holbertonschool.com>
2019-11-26 23:52:55 -0500

    [fix] typo error RATING instead RATE

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Query to list shows by ranking
-SELECT title, SUM(rate) AS rate
+SELECT title, SUM(rate) AS rating
   FROM tv_shows AS shows
        INNER JOIN tv_show_ratings AS ratings
 	   ON shows.id = ratings.show_id
- GROUP BY title ORDER BY rate DESC;
+ GROUP BY title ORDER BY rating DESC;

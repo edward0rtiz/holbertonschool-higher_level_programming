3c9473b9e4fe2b17f72e3297cc7ed9ffdd3b2daa
edward0rtiz <931@holbertonschool.com>
2019-11-26 22:23:28 -0500

    [fix] ln 6 typo

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Query for hbtn_0d_tvshows db listing all ID including NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows AS tv_shows
-	LEFT JOIN tv_show_genres AS tv_show_genres
+	 LEFT JOIN tv_show_genres AS tv_show_genres
 	    ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_shows_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

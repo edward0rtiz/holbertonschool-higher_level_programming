22c226fda460422797085d5569c2bb2ab95bbb26
edward0rtiz <931@holbertonschool.com>
2019-12-13 22:18:02 -0500

    [feature] model_state using alchemy nulable at name and base_declarative as Base

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)

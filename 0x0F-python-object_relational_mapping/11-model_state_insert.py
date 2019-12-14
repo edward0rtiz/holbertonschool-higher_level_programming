34d6b1b9cf4ee0757e884dc4dbc30509972d58e7
edward0rtiz <931@holbertonschool.com>
2019-12-13 23:36:17 -0500

    [feature] add new state using sqlalchemy session.add, session.commmit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    insert_state = State(name='Louisiana')
+    session.add(insert_state)
+    session.commit()
+    print(insert_state.id)

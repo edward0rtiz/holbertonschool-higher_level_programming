344caa8371df597d24d9b8bff47c785df4c9f5a7
edward0rtiz <931@holbertonschool.com>
2020-01-06 16:31:45 -0500

    [feature] add peak algorithm

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+
+    loi = list_of_integers
+    if loi == []:
+        return None
+    total = len(loi)
+    if total == 0:
+        return loi[0]
+    elif total == 1:
+        return max(loi)
+    middle = int(total / 2)
+    peak = loi[middle]
+    if peak > loi[middle - 1] and peak > loi[middle + 1]:
+        return peak
+    elif peak < loi[middle - 1]:
+        return find_peak(loi[:middle])
+    else:
+        return find_peak(loi[middle + 1:])

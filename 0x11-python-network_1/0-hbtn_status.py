d57b6f8b86b998a9202ad836f728d232cc03b2c0
edward0rtiz <931@holbertonschool.com>
2020-01-13 15:29:07 -0500

    [feature] add script body to fetch intranet status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__=='__main__':
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print('Body Response:')
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(the_page.decode('utf-8')))

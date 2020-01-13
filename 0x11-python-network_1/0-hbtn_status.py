e407be132ce1014b7b063cd6bc70bd88491e54dd
edward0rtiz <931@holbertonschool.com>
2020-01-13 15:33:05 -0500

    [fix] body response text

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
-        print('Body Response:')
+        print('Body response:')
         print("\t- type: {}".format(type(the_page)))
         print("\t- content: {}".format(the_page))
         print("\t- utf8 content: {}".format(the_page.decode('utf-8')))

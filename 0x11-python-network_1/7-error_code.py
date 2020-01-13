ba1de6407f7f546b9c850c54cbc8560ca456644b
edward0rtiz <931@holbertonschool.com>
2020-01-13 17:29:44 -0500

    [fix] pep8 whitespace ln 12

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     url = sys.argv[1]
     req = requests.get(url)
-    if req.status_code >=400:
+    if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
     else:
         print(req.text)

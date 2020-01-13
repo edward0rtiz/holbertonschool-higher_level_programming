32800e82db499dbd6789d78393241e5237b6f7cc
edward0rtiz <931@holbertonschool.com>
2020-01-13 16:54:06 -0500

    [fix] pep8 ln2 too long

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in a URL, sends a request to the URL and displays the value
+"""script that takes a URL, sends a request to the URL and displays the value
 of the variable X-Request-Id in the response header"""
 
 import sys

c3b4e3c3fa86d8ffd033dca0e5a5c083547eaa08
edward0rtiz <931@holbertonschool.com>
2020-01-13 16:16:11 -0500

    [fix] decode utf-8

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:
-            print(response.read().decode('ascii'))
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))

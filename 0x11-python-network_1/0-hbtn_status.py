0756b233abb1e2b270c198534921e5ef1811c79b
edward0rtiz <931@holbertonschool.com>
2020-01-13 15:32:00 -0500

    [fix] pep8 ln5 space 12

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,8 @@
 """Write a Python script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-if __name__=='__main__':
+if __name__ == '__main__':
+
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         the_page = response.read()

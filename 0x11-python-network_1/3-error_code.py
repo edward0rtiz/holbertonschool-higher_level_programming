e220542aef795ecb6b6ed5afe7c0f80479fc3547
edward0rtiz <931@holbertonschool.com>
2020-01-13 16:21:08 -0500

    [fix] remove spaceline and add permissions

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,6 @@ import urllib.request
 if __name__ == '__main__':
 
     url = sys.argv[1]
-    
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:

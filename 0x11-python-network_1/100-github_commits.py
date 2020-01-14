91b8caefb929f0791048587b3482f6f8ffbde5bb
edward0rtiz <931@holbertonschool.com>
2020-01-13 20:09:18 -0500

    [fix] argv order user first and repo next

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
 
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[1], sys.argv[2])
+        sys.argv[2], sys.argv[1])
     r = requests.get(url)
     commits = r.json()
     try:

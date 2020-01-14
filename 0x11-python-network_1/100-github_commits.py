39d94b9b1eb18cd90cedbfba8c4f0ab3b195d4ab
edward0rtiz <931@holbertonschool.com>
2020-01-13 20:06:09 -0500

    [fix] pep8 ln too long

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,12 +6,14 @@ import sys
 
 if __name__ == '__main__':
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1], sys.argv[2])
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[1], sys.argv[2])
     r = requests.get(url)
     commits = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(commits[i].get('sha'),
-                                  commits[i].get('commit').get('author').get('name')))
+            print("{}: {}".format(
+                commits[i].get('sha'),
+                commits[i].get('commit').get('author').get('name')))
     except IndexError:
         pass

bec911254990c31e1024f40f8fd036f8e64eccb8
edward0rtiz <931@holbertonschool.com>
2020-01-13 19:15:28 -0500

    [feature] use githubapi to extract id of username and passwd

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes your Github credentials (username and password)
+and uses the Github API to display your id"""
+
+import sys
+from requests.auth import HTTPBasicAuth
+import requests
+
+if __name__ == '__main__':
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get('https://api.github.com/user', auth=auth)
+    print(r.json().get('id'))

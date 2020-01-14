f91f0029ac4fb92a4681f6003339fdac1216ae31
edward0rtiz <931@holbertonschool.com>
2020-01-13 21:25:57 -0500

    [feature] sending a search request to api of starwars using pagination

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that takes in a string and sends a search request to the SW API"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    url = 'https://swapi.co/api/people'
+    params = {'search': sys.argv[1]}
+    result = requests.get(url, params=params).json()
+    count = result.get("count")
+    print("Number of results: {}".format(count))
+
+    page = 0
+    while page < count:
+        for r in result.get('results'):
+            print(r.get('name'))
+            page +=1
+        next_page = result.get('next')
+        if next_page is not None:
+            results = requests.get(next_page).json()

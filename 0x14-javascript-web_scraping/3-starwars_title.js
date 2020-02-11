64b0b40da877b7717891c4d180ad1c0069fc7b2a
edward0rtiz <931@holbertonschool.com>
2020-02-11 13:43:31 -0500

    [fix] semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (err, httpResponse, body) {
-    console.log(JSON.parse(body).title || err);
+  console.log(JSON.parse(body).title || err);
 });

411df6cd220f60f99a1c8eb40dd3c6f9f3e8952c
edward0rtiz <931@holbertonschool.com>
2020-02-11 13:31:31 -0500

    [fix] function error data

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  console.log(data || err);
 });

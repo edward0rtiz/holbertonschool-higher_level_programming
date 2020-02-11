dd2583f39686e361be7e1cb582e1d4de80a14626
edward0rtiz <931@holbertonschool.com>
2020-02-11 13:35:37 -0500

    [fix] error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, content) {
-  console.log(content || err);
+fs.readFile(process.argv[2], 'utf-8', function (error, content) {
+  console.log(error || content);
 });

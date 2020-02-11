69b952aa395bbdc0f72c7477d619590da4806241
edward0rtiz <931@holbertonschool.com>
2020-02-11 09:56:24 -0500

    [feature] count the number go movies using for loop and concat

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+
+request(url, function (err, httpResponse, body) {
+  if (err);
+  console.log(err);
+  let list = [];
+  for (const film of JSON.parse(body).results) {
+    list = list.concat(film.characters);
+  }
+  const uniq = list.filter(x => x.includes('18'));
+  console.log(uniq.length);
+});

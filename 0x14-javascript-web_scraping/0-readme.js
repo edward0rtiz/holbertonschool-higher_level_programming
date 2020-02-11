59ee7867a1b9e8a07b6e042ccdc7e31ebcfb8448
edward0rtiz <931@holbertonschool.com>
2020-02-11 07:42:30 -0500

    [fix] semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
+  if (err) throw err;
+  console.log(data);
 });

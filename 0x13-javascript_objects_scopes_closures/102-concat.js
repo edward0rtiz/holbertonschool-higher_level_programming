48b4c9140f0061c793e7d956337d8385240033a1
edward0rtiz <931@holbertonschool.com>
2020-02-11 07:04:57 -0500

    [fix] semistandard

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,4 +2,4 @@
 const fs = require('fs');
 const f1 = fs.readFileSync(process.argv[2]);
 const f2 = fs.readFileSync(process.argv[3]);
-fs.writeFileSync(process.argv[4], f1 + f2, 'utf-8' );
+fs.writeFileSync(process.argv[4], f1 + f2, 'utf-8');

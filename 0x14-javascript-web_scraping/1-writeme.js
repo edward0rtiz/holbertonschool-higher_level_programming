56c42d40a571ce497274f91632d17361dfcab498
edward0rtiz <931@holbertonschool.com>
2020-02-11 13:39:58 -0500

    [fix] remove utf-8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-  if (err) throw err;
-  console.log(err);
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
+  if (err) console.log(err);
 });
